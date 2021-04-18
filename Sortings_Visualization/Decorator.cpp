68f22648f8dd1234fdb45b8f0b8f7a592ddd2aa2
alekseykondus <60347730+alekseykondus@users.noreply.github.com>
2021-04-18 18:23:26 +0200

    fixed bug with time

--- Sortings_Visualization/Decorator.cpp
+++ Sortings_Visualization/Decorator.cpp
@@ -115,5 +115,5 @@ void SortingAndTiming::SetSorting(Sortings::SortingName name){
 qint64 SortingAndTiming::Run(std::vector<int>& m_Numbers){
     time.start();
     m_Sorting->Sort(m_Numbers.begin(), m_Numbers.end(), [](int x, int y) { return x < y; });
-    return time.nsecsElapsed();
+    return time.elapsed();
 }
