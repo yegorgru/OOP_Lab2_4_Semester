c20726fda01f1856495093d1b7693bc5abc9df31
alekseykondus <60347730+alekseykondus@users.noreply.github.com>
2021-04-22 23:19:26 +0200

    worked on theoretical complexity

--- Sortings_Visualization/Decorator.h
+++ Sortings_Visualization/Decorator.h
@@ -28,37 +28,84 @@ namespace Sortings{
             return (float)time.nsecsElapsed()/1000000;
         }
 
-        std::string ComplexityCheck(Sortings::SortingName name){
+        std::vector<QString> ComplexityCheck(Sortings::SortingName name){
+            std::vector<QString> performance;
             if ((name == Sortings::SortingName::BUBBLESORT ) || (name == Sortings::SortingName::INSERTIONSORT) ||
-                    (name == Sortings::SortingName::SELECTIONSORT) || (name == Sortings::SortingName::SHAKERSORT) || (name == Sortings::SortingName::GNOMESORT)
-                    || (name == Sortings::SortingName::ODDEVENSORT) || (name == Sortings::SortingName::PANCAKESORT) || (name == Sortings::SortingName::STOOGESORT))
+                    (name == Sortings::SortingName::SELECTIONSORT) || (name == Sortings::SortingName::SHAKERSORT) ||
+                    (name == Sortings::SortingName::GNOMESORT) || (name == Sortings::SortingName::ODDEVENSORT) ||
+                    (name == Sortings::SortingName::PANCAKESORT) )
             {
-                return "O(n²)";
+                performance.push_back("Best-case performance: O(n)");
+                performance.push_back("Average performance: O(n²)");
+                performance.push_back("Worst-case performance: O(n²)");
             }
-            else if ((name == Sortings::SortingName::COMBSORT) || (name == Sortings::SortingName::QUICKSORT) || (name == Sortings::SortingName::MERGESORT)
-                       || (name == Sortings::SortingName::MERGESORTINPLACE) || (name == Sortings::SortingName::HEAPSORT) || (name == Sortings::SortingName::TIMSORT)
-                       || (name == Sortings::SortingName::INTROSORT) || (name == Sortings::SortingName::SHELLSORT) || (name == Sortings::SortingName::BUCKETSORT))
+            else if ( (name == Sortings::SortingName::SELECTIONSORT) ) {
+                performance.push_back("Best-case performance: O(n²)");
+                performance.push_back("Average performance: O(n²)");
+                performance.push_back("Worst-case performance: O(n²)");
+            }
+            else if ( (name == Sortings::SortingName::STOOGESORT) ) {
+                performance.push_back("Best-case performance: ---");
+                performance.push_back("Average performance: ---");
+                performance.push_back("Worst-case performance: ---");
+            }
+            else if (name == Sortings::SortingName::COMBSORT)
             {
-                return "O(n log(n))";
+                performance.push_back("Best-case performance: O(n log(n))");
+                performance.push_back("Average performance: O(n²/2^p)");
+                performance.push_back("Worst-case performance: O(n²)");
             }
-            else if((name == Sortings::SortingName::PIGEONHOLESORT) || (name == Sortings::SortingName::COUNTINGSORT) || (name == Sortings::SortingName::RADIXSORT)
-                      )
-            {
-                return "O(n + Range)";
+            else if ((name == Sortings::SortingName::QUICKSORT) || (name == Sortings::SortingName::SHELLSORT)) {
+                performance.push_back("Best-case performance: O(n log(n))");
+                performance.push_back("Average performance: O(n log(n))");
+                performance.push_back("Worst-case performance: O(n²)");
             }
-            else if(name == Sortings::SortingName::FLASHSORT)
-            {
-                return "O(n)";
+            else if (name == Sortings::SortingName::TIMSORT) {
+                performance.push_back("Best-case performance: O(n)");
+                performance.push_back("Average performance: O(n log(n))");
+                performance.push_back("Worst-case performance: O(n log(n))");
             }
-            else if(name == Sortings::SortingName::BOGOSORT)
-            {
-                return "O((n+1)!)";
+            else if (name == Sortings::SortingName::BUCKETSORT) {
+                performance.push_back("Best-case performance: O(n)");
+                performance.push_back("Average performance: O(n log(n))");
+                performance.push_back("Worst-case performance: O(n²)");
             }
-            else if(name == Sortings::SortingName::BOGOSORT)
-            {
-                return "O(>n²)";
+            else if ((name == Sortings::SortingName::MERGESORT) || (name == Sortings::SortingName::MERGESORTINPLACE) ||
+                     (name == Sortings::SortingName::HEAPSORT) || (name == Sortings::SortingName::INTROSORT)) {
+                performance.push_back("Best-case performance: O(n log(n))");
+                performance.push_back("Average performance: O(n log(n))");
+                performance.push_back("Worst-case performance: O(n log(n))");
             }
-            return "Unrecognised";
+
+            else if((name == Sortings::SortingName::PIGEONHOLESORT) || (name == Sortings::SortingName::COUNTINGSORT)) {
+                performance.push_back("Best-case performance: O(n + Range)");
+                performance.push_back("Average performance: O(n + Range)");
+                performance.push_back("Worst-case performance: O(n + Range)");
+            }
+            else if (name == Sortings::SortingName::RADIXSORT) {
+                performance.push_back("Best-case performance: O(n * Range)");
+                performance.push_back("Average performance: O(n + Range)");
+                performance.push_back("Worst-case performance: O(n + Range)");
+            }
+
+            else if(name == Sortings::SortingName::FLASHSORT) {
+                performance.push_back("Best-case performance: O(n)");
+                performance.push_back("Average performance: O(n + Range)");
+                performance.push_back("Worst-case performance: O(n²)");
+            }
+            else if(name == Sortings::SortingName::BOGOSORT) {
+                performance.push_back("Best-case performance: O(n)");
+                performance.push_back("Average performance: O((n+1)!");
+                performance.push_back("Worst-case performance: O((n+1)!");
+            }
+            else if(name == Sortings::SortingName::SLOWSORT) {
+                performance.push_back("Best-case performance: O(n^((log 2 n) / (2 + ε))");
+                performance.push_back("Average performance: O(n^((log 2 n) / (2 + ε))");
+                performance.push_back("Worst-case performance: O^((n(log 2 n) / (2 + ε))");
+            }
+            else
+                performance.push_back("Unrecognised");
+            return performance;
         }
 
     private:
